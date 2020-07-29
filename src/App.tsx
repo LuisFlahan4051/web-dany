8c58d3a951a51fc24b73989a6fd57a2f6978b725
LuisFlahan4051 <luisflahan4051@gmail.com>
2020-07-29 04:09:53 -0500

    Se afinaron detalles y se agregaron archivos al workspace

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      Hello world! Por agregar elementos...
+      {/* clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%); <-- CSS */}
+      <svg>
+        <path d="M0.00,49.98 C149.99,150.00 271.49,-49.98 500.00,49.98 L500.00,0.00 L0.00,0.00 Z" fill='#00f6ff'/>
+      </svg>
+      
+      
       <div className="animacion"></div>
     </div>
   );
